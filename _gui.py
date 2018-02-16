f3ba4c66eefb5d50cb655c3cb5ba9a8de27d5b90
Pira <lucas.pires@tubanharon.com>
2018-02-16 20:35:44 -0200

    gui is sort-of working.

--- _gui.py
+++ _gui.py
@@ -6,6 +6,9 @@ from Material import *
 from UnitOp import *
 
 
+water = FlowStream(['water'], wi=[1.0], Mf=1.0, T=25+273)
+
+
 #region INTERFACE
 
 from tkinter import *
@@ -72,27 +75,32 @@ maximum = max(tanque.D, Ht)
 #endregion
 
 #region GRAPHS
+
 class Graph(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         fl = Figure(figsize=(5, 5), dpi=100)
 
         #region DEFINITIONS
+
         tb_D.insert(0, tanque.D)
         tb_H.insert(0, tanque.H)
         tb_angle.insert(0, tanque.Theta)
         tb_tf.insert(0, 100)
         tb_Vo.insert(0, tanque.InitialVolume)
         tb_ofr.insert(0, 0.0)
+
         #endregion
 
         #region TANK PLOT
+
         xTk = [0, -D / 2, -D / 2, +D / 2, +D / 2, 0]
         yTk = [0, Hc, Ht, Ht, Hc, 0]
         ax = fl.add_subplot(111)
         ax.plot(xTk, yTk, color='black')
         ax.set_xlim(-maximum * 0.75, +maximum * 0.75)
         ax.set_ylim(0, maximum * 1.5)
+
         #endregion
 
         canvas = FigureCanvasTkAgg(fl, self)
@@ -109,6 +117,7 @@ class DynamicGraph(Frame):
         fl = Figure(figsize=(5, 5), dpi=100)
 
         #region DEFINITIONS
+
         tanque.D = float(tb_D.get())
         tanque.H = float(tb_H.get())
         tanque.Theta = float(tb_angle.get())
@@ -118,14 +127,15 @@ class DynamicGraph(Frame):
         Ht = tanque.TankHeight
         Hs = tanque.TankHeight * 1.3
         maximum = max(tanque.D, Ht)
+        h_list = tanque.LiquidHeights([water], outlet_list, t_list, return_array=True)
+        d_list = tanque.LiquidDiameters([water], outlet_list, t_list, return_array=True)
+        x_list = [tuple([-d / 2, d / 2]) for d in d_list]
+        y_list = [tuple([h, h]) for h in h_list]
 
-        # h_list = tanque.LiquidHeights([water], outlet_list, t_list, return_array=True)
-        # d_list = tanque.LiquidDiameters([water], outlet_list, t_list, return_array=True)
-        # x_list = [tuple([-d / 2, d / 2]) for d in d_list]
-        # y_list = [tuple([h, h]) for h in h_list]
         #endregion
 
         # region STATIC PLOTS
+
         # cylTank #
         xTk = [0, -D / 2, -D / 2, +D / 2, +D / 2, 0]
         yTk = [0, Hc, Ht, Ht, Hc, 0]
@@ -134,49 +144,53 @@ class DynamicGraph(Frame):
         ax.set_xlim(-maximum * 0.75, +maximum * 0.75)
         ax.set_ylim(0, maximum * 1.5)
         # Shower #
-        # xS = [0]
-        # yS = [Hs]
-        # ax.scatter(xS, yS, s=100.0, c='gray')
-        # endregion
+        xS = [0]
+        yS = [Hs]
+        ax.scatter(xS, yS, s=100.0, c='gray')
+        endregion
 
         #region DYNAMIC PLOTS
-        # for i, t in enumerate(t_list):
-        #     # Liquid Level #
-        #     if tanque.Theta > 0.0:
-        #         ax.plot(x_list[i], y_list[i], color='cyan')
-        #     else:
-        #         ax.stackplot(x_list[i], y_list[i], color='cyan')
-            # Droplets #
-            # xD = [0, 0, 0, 0, 0]
-            # xD1 = np.linspace(0, -0.05, 5)
-            # xD2 = np.linspace(0, +0.05, 5)
-            # yD = [0.1 * Ht + 0.9 * Hs, 0.3 * Ht + 0.7 * Hs, 0.5 * Ht + 0.5 * Hs, 0.7 * Ht + 0.3 * Hs,
-            #       0.9 * Ht + 0.1 * Hs]
-            # yyD = [Hs, 0.2 * Ht + 0.8 * Hs, 0.4 * Ht + 0.6 * Hs, 0.6 * Ht + 0.4 * Hs, 0.8 * Ht + 0.2 * Hs]
-            # a = ax.scatter(xD, yD, s=20.0, c='cyan')
-            # a1 = ax.scatter(xD1, yD, s=10.0, c='cyan')
-            # a2 = ax.scatter(xD2, yD, s=10.0, c='cyan')
-            # ax.draw()
-            # ax.pause(0.001)
-            # a.remove(), a1.remove(), a2.remove()
-            # aa = ax.scatter(xD, yyD, s=20.0, c='cyan')
-            # aa1 = ax.scatter(xD1, yyD, s=10.0, c='cyan')
-            # aa2 = ax.scatter(xD2, yyD, s=10.0, c='cyan')
-            # ax.draw()
-            # ax.pause(0.001)
-            # aa.remove(), aa1.remove(), aa2.remove()
+
+        for i, t in enumerate(t_list):
+             # liquid level #
+             if tanque.theta > 0.0:
+                 ax.plot(x_list[i], y_list[i], color='cyan')
+             else:
+                 ax.stackplot(x_list[i], y_list[i], color='cyan')
+            # droplets #
+             xd = [0, 0, 0, 0, 0]
+             xd1 = np.linspace(0, -0.05, 5)
+             xd2 = np.linspace(0, +0.05, 5)
+             yd = [0.1 * ht + 0.9 * hs, 0.3 * ht + 0.7 * hs, 0.5 * ht + 0.5 * hs, 0.7 * ht + 0.3 * hs,
+                   0.9 * ht + 0.1 * hs]
+             yyd = [hs, 0.2 * ht + 0.8 * hs, 0.4 * ht + 0.6 * hs, 0.6 * ht + 0.4 * hs, 0.8 * ht + 0.2 * hs]
+             a = ax.scatter(xd, yd, s=20.0, c='cyan')
+             a1 = ax.scatter(xd1, yd, s=10.0, c='cyan')
+             a2 = ax.scatter(xd2, yd, s=10.0, c='cyan')
+             ax.draw()
+             ax.pause(0.001)
+             a.remove(), a1.remove(), a2.remove()
+             aa = ax.scatter(xd, yyd, s=20.0, c='cyan')
+             aa1 = ax.scatter(xd1, yyd, s=10.0, c='cyan')
+             aa2 = ax.scatter(xd2, yyd, s=10.0, c='cyan')
+             ax.draw()
+             ax.pause(0.001)
+             aa.remove(), aa1.remove(), aa2.remove()
 
         canvas = FigureCanvasTkAgg(fl, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+
         #endregion
 
         toolbar = NavigationToolbar2TkAgg(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=BOTTOM, fill=BOTH, expand=True)
+
 #endregion
 
-#region FUNCTIONS
+#region FUNCTIONS:
+
 def onChange(sv):
     DynamicGraph(root).grid(row=2, column=4, rowspan=8)
 
@@ -188,6 +202,7 @@ def Run(event):
 
     tanque.Animation([water], outlet_list, t_list)
     plt.close()
+
 #endregion
 
 Graph(root).grid(row=2, column=4, rowspan=8)
